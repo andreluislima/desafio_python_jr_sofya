14612cf469c055a774ff7a276c8ecaff403d56c3
andreluislima <andrelimag89@gmail.com>
2026-01-01 15:00:46 -0300

    fix(schemas:appointment): cria validacao de complaint.

--- app/schemas/consultations/appointment.py
+++ app/schemas/consultations/appointment.py
@@ -5,14 +5,22 @@ from pydantic import BaseModel, Field, field_validator
 
 class AppointmentIn(BaseModel):
     date: date  
-    complaint:str = Field(..., min_length=3)
+    complaint:str = Field(...)
     notes: Optional[str] = None
 
     @field_validator("date")
     @classmethod
     def validar_data_consulta(cls, value:date) -> date:
         if value > date.today():
-            raise ValueError("Data inválida.\nData de consulta não pode ser maior que a data de hoje.")
+            raise ValueError("Data inválida. Data de consulta não pode ser maior que a data de hoje.")
+        return value
+
+    @field_validator("complaint")
+    @classmethod
+    def validar_complaint(cls, value:str) -> str:
+        value = value.strip()
+        if len(value) < 3:
+            raise ValueError("Sintoma do paciente é obrigatório e deve ter no mínimo 3 caracteres.")
         return value
 
 class AppointmentOut(BaseModel):
